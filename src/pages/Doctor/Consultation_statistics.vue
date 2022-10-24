f6bdb66d00e968ceccd67007f9c66ff8bacc2a9b
WillyValentin <wvaleric@gmail.com>
2022-10-24 12:04:11 -0500

    Home - connected

---
+++ src/pages/Doctor/Consultation_statistics.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div style="margin-left: 10%; margin-top:15%; margin-right: 10%; margin-bottom: 15%; border: black 3px solid">
+            <h1 style="text-align: center;">Estadísticas de Consultas</h1>
+            <br><br>
+            <div class=" d-flex justify-content-evenly ">
+                <div class="container" style="width:35% ;">
+                    <canvas id="myChart"></canvas>
+                </div>
+                <div class="container" style="width:35% ;">
+                    <canvas id="myChart2"></canvas>
+                </div>
+            </div>
+            <br><br>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default 
+{
+
+}
+
+</script>
+    import { Bar } from 'vue-chartjs'
+<style>
+
+</style>
--- src/pages/loggin/Loggin.vue
+++ src/pages/loggin/Loggin.vue
@@ -22,8 +22,10 @@ import Loggin from '@/pages/loggin/Loggin.vue';
 </template>
 
 <script>
+import router from '@/router/router';
 import { DoctorServices } from '@/services/DoctorServices'
-
+import { useRoute, useRouter } from "vue-router";
+import { Home } from '@/pages/Doctor/Home.vue'
 export default {
     data() {
         return {
@@ -37,11 +39,12 @@ export default {
     },
     methods: {
         async loggin() {
-            this.doctorid = await this.doctorServices.validateDoctor(this.username, this.password)
+            this.doctorid = await this.doctorServateDoctor(this.username, this.password)
             if(this.doctorid) {
                 console.log(this.doctorid)
                 this.username = ''
                 this.password = ''
+                router.push(`/doctors/:${this.doctorid.id}`)
                 //return true
             }
             console.log('No')
